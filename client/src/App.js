5d77d31a01765a70840bc58b5f0253131ddcaa19
kkaps002 <60240943+kkaps002@users.noreply.github.com>
2024-08-08 13:16:10 +0100

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
+import Nav from "./components/Nav.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <Nav />
+      <div className="p-4">hello</div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
