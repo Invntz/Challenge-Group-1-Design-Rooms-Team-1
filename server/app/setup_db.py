5a7d2bdcf07baba1bda992aa0f9fc1ec644eeba7
Cara McCrossan <cara.mccrossan@icloud.com>
2024-08-08 15:52:41 +0100

    Added signup router

--- server/app/setup_db.py
+++ server/app/setup_db.py
@@ -8,8 +8,8 @@ cursor.execute(
 CREATE TABLE IF NOT EXISTS user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(30) NOT NULL
-    firstname VARCHAR(30) NOT NULL
+    password VARCHAR(30) NOT NULL,
+    firstname VARCHAR(30) NOT NULL,
     surname VARCHAR(30) NOT NULL
 )
 """
