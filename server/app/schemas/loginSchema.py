f3ba8f2e2264b29c243230cac60360abef659664
Tony Hoong <tonyhoong40@gmail.com>
2024-08-08 20:46:18 +0100

    setup jwt stuff

--- server/app/schemas/loginSchema.py
+++ server/app/schemas/loginSchema.py
@@ -1,1 +1,13 @@
 from pydantic import BaseModel
+
+
+class LoginBase(BaseModel):
+    username: str
+    password: str
+
+
+class Login(LoginBase):
+    id: int
+
+    class Config:
+        orm_mode = True
