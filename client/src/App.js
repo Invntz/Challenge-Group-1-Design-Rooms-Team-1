2795a532002776d9bfbb3f75fac076ddbc96f9ec
kkaps002 <60240943+kkaps002@users.noreply.github.com>
2024-08-08 13:36:23 +0100

    merge issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Nav from "./components/Nav.js";
 import "./App.css";
@@ -8,12 +7,6 @@ function App() {
     <div className="App">
       <Nav />
       <div className="p-4">hello</div>
-=======
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
->>>>>>> 2f0a929a4d3d63b1452d6c8cca17db9056a73073
     </div>
   );
 }
